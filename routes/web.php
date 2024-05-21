01cf59c509059228cb03139618b92ca8f27169d2
wchynto <wchynto.dev@gmail.com>
2024-05-21 21:08:38 +0700

    fetch merge from 'develop'

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-  return view('home', ['title' => 'Home - Pantai Goa Petapa']);
+    return view('home', ['title' => 'Home - Pantai Goa Petapa']);
 });
 
 Route::get('/ticket', function () {
-  return view('ticket', ['title' => 'Ticket - Pantai Goa Petapa']);
+    return view('ticket', ['title' => 'Ticket - Pantai Goa Petapa']);
 });
 
 Route::get('/about', function () {
-  return view('about', ['title' => 'About - Pantai Goa Petapa']);
+    return view('about', ['title' => 'About - Pantai Goa Petapa']);
 });
 
 Route::get('/contact', function () {
-  return view('contact', ['title' => 'Contact - Pantai Goa Petapa']);
+    return view('contact', ['title' => 'Contact - Pantai Goa Petapa']);
 });
 
 // Admin routes
 Route::get('/dashboard', function () {
-  return view('dashboard');
+    return view('dashboard');
 })->middleware(['auth'])->name('dashboard');

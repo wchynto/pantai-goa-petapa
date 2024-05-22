692d7d89e0861bcf3ac42fcdf2e59e1eca260acd
Irfan Dwi Samudra <irfandwisamudra08@gmail.com>
2024-05-22 12:07:13 +0700

    Update routes web.php

--- routes/web.php
+++ routes/web.php
@@ -4,29 +4,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
   return view('home', ['title' => 'Home - Pantai Goa Petapa']);
-  return view('home', ['title' => 'Home - Pantai Goa Petapa']);
 });
 
 Route::get('/ticket', function () {
-  return view('ticket', ['title' => 'Ticket - Pantai Goa Petapa']);
   return view('ticket', ['title' => 'Ticket - Pantai Goa Petapa']);
 });
 
 Route::get('/about', function () {
   return view('about', ['title' => 'About - Pantai Goa Petapa']);
-  return view('about', ['title' => 'About - Pantai Goa Petapa']);
 });
 
 Route::get('/contact', function () {
-  return view('contact', ['title' => 'Contact - Pantai Goa Petapa']);
-});
-
-Route::get('/login', function () {
-  return view('login', ['title' => 'Login - Pantai Goa Petapa']);
-});
-
-Route::get('/sign-up', function () {
-  return view('sign-up', ['title' => 'Sign Up - Pantai Goa Petapa']);
   return view('contact', ['title' => 'Contact - Pantai Goa Petapa']);
 });
 

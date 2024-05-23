12c16f8c4129b68122e1718be11a3ebf3192f59c
Irfan Dwi Samudra <irfandwisamudra08@gmail.com>
2024-05-24 01:59:16 +0700

    Update chart labels and values to use Indonesian language

--- resources/views/components/admin/statistical-numbers.blade.php
+++ resources/views/components/admin/statistical-numbers.blade.php
@@ -3,20 +3,20 @@
   <div class="max-w-screen-xl mx-auto text-center">
     <dl class="grid gap-8 sm:gap-0 mx-auto text-gray-900 sm:grid-cols-4 dark:text-white">
       <div class="flex flex-col items-center justify-center">
-        <dt class="mb-2 text-3xl md:text-4xl font-extrabold">8.6M</dt>
-        <dd class="font-light text-gray-500 dark:text-gray-400">Total Profits</dd>
+        <dt class="mb-2 text-3xl md:text-4xl font-extrabold">8,6 Jt</dt>
+        <dd class="font-light text-gray-500 dark:text-gray-400">Total Keuntungan</dd>
       </div>
       <div class="flex flex-col items-center justify-center">
         <dt class="mb-2 text-3xl md:text-4xl font-extrabold">2.340</dt>
-        <dd class="font-light text-gray-500 dark:text-gray-400">Total Visitors</dd>
+        <dd class="font-light text-gray-500 dark:text-gray-400">Total Pengunjung</dd>
       </div>
       <div class="flex flex-col items-center justify-center">
         <dt class="mb-2 text-3xl md:text-4xl font-extrabold">4</dt>
-        <dd class="font-light text-gray-500 dark:text-gray-400">Total Tickets</dd>
+        <dd class="font-light text-gray-500 dark:text-gray-400">Total Tiket</dd>
       </div>
       <div class="flex flex-col items-center justify-center">
         <dt class="mb-2 text-3xl md:text-4xl font-extrabold">1.456</dt>
-        <dd class="font-light text-gray-500 dark:text-gray-400">Total Users</dd>
+        <dd class="font-light text-gray-500 dark:text-gray-400">Total Members</dd>
       </div>
     </dl>
   </div>

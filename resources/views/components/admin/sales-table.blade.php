8e2309bffeaffeedd7a81a7103ad1321bde611ec
Irfan Dwi Samudra <irfandwisamudra08@gmail.com>
2024-05-26 09:19:47 +0700

    Add delete transaction functionality and modal

--- resources/views/components/admin/sales-table.blade.php
+++ resources/views/components/admin/sales-table.blade.php
@@ -161,15 +161,16 @@
                   @endif
                 </td>
                 <td class="p-4 whitespace-nowrap">
-                  <button
-                    class="text-xs font-medium text-blue-600 dark:text-blue-400 hover:text-blue-500 dark:hover:text-blue-300">
+                  {{-- Detail Button --}}
+                  <a href="{{ url('admin/sales/' . $item['transaksi_id']) }}"
+                    class="text-xs font-medium text-blue-600 dark:text-blue-400 hover:text-blue-500 dark:hover:text-blue-300 inline-block">
                     <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
                       height="24" fill="none" viewBox="0 0 24 24">
                       <path stroke="currentColor" stroke-width="2"
                         d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
                       <path stroke="currentColor" stroke-width="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     </svg>
-                  </button>
+                  </a>
 
                   {{-- Edit Button --}}
                   <button type="button"
@@ -293,14 +294,64 @@
                       </div>
                     </div>
                   </div>
+
+                  {{-- Delete Button --}}
                   <button
-                    class="text-xs font-medium text-red-600 dark:text-red-400 hover:text-red-500 dark:hover:text-red-300">
+                    class="text-xs font-medium text-red-600 dark:text-red-400 hover:text-red-500 dark:hover:text-red-300"
+                    data-modal-target="deleteTransaksiModal-{{ $item['transaksi_id'] }}"
+                    data-modal-toggle="deleteTransaksiModal-{{ $item['transaksi_id'] }}">
                     <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
                       height="24" fill="none" viewBox="0 0 24 24">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
                     </svg>
                   </button>
+                  {{-- Delete Transksi Modal --}}
+                  <div
+                    class="fixed left-0 right-0 z-50 items-center justify-center hidden overflow-x-hidden overflow-y-auto top-4 md:inset-0 h-modal sm:h-full"
+                    id="deleteTransaksiModal-{{ $item['transaksi_id'] }}">
+                    <div class="relative w-full h-full max-w-md px-4 md:h-auto">
+                      <!-- Modal content -->
+                      <div class="relative bg-white rounded-lg shadow dark:bg-gray-800">
+                        <!-- Modal header -->
+                        <div class="flex justify-end p-2">
+                          <button type="button"
+                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-700 dark:hover:text-white"
+                            data-modal-hide="deleteTransaksiModal-{{ $item['transaksi_id'] }}">
+                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                              xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd"
+                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                clip-rule="evenodd"></path>
+                            </svg>
+                          </button>
+                        </div>
+                        <!-- Modal body -->
+                        <form class="p-4 md:p-5" method="POST">
+                          <input type="hidden" name="transaksi_id" value="{{ $item['transaksi_id'] }}">
+                          <div class="p-6 pt-0 text-center">
+                            <svg class="w-16 h-16 mx-auto text-red-600" fill="none" stroke="currentColor"
+                              viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                            </svg>
+                            <h3 class="mt-5 mb-6 text-lg text-gray-500 dark:text-gray-400 whitespace-normal">
+                              Apakah Anda yakin ingin menghapus transaksi ini?
+                            </h3>
+                            <button type="submit"
+                              class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2.5 text-center mr-2 dark:focus:ring-red-800">
+                              Yes, I'm sure
+                            </button>
+                            <a href="#"
+                              class="text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2.5 text-center dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+                              data-modal-hide="deleteTransaksiModal-{{ $item['transaksi_id'] }}">
+                              No, cancel
+                            </a>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
                 </td>
               </tr>
             @endforeach

bef9b00fe7419bc31dde8bb4d513f2227e006136
NurulRizka <nurulrizka222@gmail.com>
2024-06-01 21:49:13 +0700

    update confirmation

--- resources/views/confirmation-order.blade.php
+++ resources/views/confirmation-order.blade.php
@@ -32,40 +32,74 @@
           <h1 class="text-l mb-4 font-semibold">Konfirmasi Pemesanan</h1>
           <div class="w-full bg-blue-200 border border-gray-200 shadow-xl sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-600 my-4 rounded-lg py-4">
             {{-- METODE PEMBAYARAN --}}
-            <h1 class="text-l mb-4 font-semibold dark:text-white">Metode Pembayaran</h1>
-            <div class="flex flex-col sm:flex-row gap-10">
-              <div class="w-1/3 bg-white dark:bg-white rounded-lg l:py-1 ">
+            <h1 class="text-l font-semibold dark:text-white">Metode Pembayaran</h1>
+            <div class="flex flex-col sm:flex-row gap-10 my-4">
+              <div class="w-full shadow border border-gray-300 bg-white dark:bg-gray-700 dark:border-gray-600 rounded-lg ">
                 <img src="{{ asset('images/dana.png') }}" alt="Hero Pantai Goa Petapa" class="mx-auto py-2">
               </div>
-              <div class="w-1/3 bg-white dark:bg-white rounded-lg l:py-1">
+              <div class="w-full shadow border border-gray-300 bg-white dark:bg-gray-700 dark:border-gray-600 rounded-lg">
                 <img src="{{ asset('images/bank.png') }}" alt="Hero Pantai Goa Petapa" class="mx-auto py-2">
               </div>
-              <div class="w-1/3 bg-white dark:bg-white rounded-lg l:py-1">
+              <div class="w-full shadow border border-gray-300 bg-white dark:bg-gray-700 dark:border-gray-600 rounded-lg">
                 <img src="{{ asset('images/dana.png') }}" alt="Hero Pantai Goa Petapa" class="mx-auto py-2">
               </div>
             </div>
-
+            
             {{-- INFO AKUN --}}
-            <h1 class="text-l mb-4 font-semibold dark:text-white">Informasi Akun</h1>
-            <div class="w-2/4"></div>
-            <div class="w-1/4"></div>
-            <div class="w-1/4"></div>
+            <h1 class="text-l font-semibold dark:text-white">Informasi Akun</h1>
+            <div class="flex flex-col sm:flex-row gap-10 my-4">
+              <div class="w-full sm:w-2/3 rounded-lg">
+                <input type="text" name="card_number" id="card_number" class="w-full bg-white shadow border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Enter your virtual account number" required="">
+              </div>
+              <div class="w-full sm:w-1/3 rounded-lg l:py-1 ">
+                <input type="text" name="usn_dana" id="usn_dana"
+                      class="bg-white shadow border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Username Dana" required="">
+              </div>
+            </div>
 
-            {{-- INFO PENGIRIMAN --}}
-            <h1 class="text-l mb-4 font-semibold dark:text-white">Informasi Pengiriman</h1>
-            <div class="w-2/5"></div>
-            <div class="w-3/5"></div>
+            {{-- LOKASI MAP --}}
+            <h1 class="text-l mb-4 font-semibold dark:text-white">Lokasi Wisata</h1>
+            <div class="flex flex-col sm:flex-row gap-10 my-4">
+              {{-- MAPS --}}
+              <div class="w-full sm:w-1/6 lg:w-full aspect-w-16 aspect-h-9">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15834.903932784351!2d112.79205323181527!3d-7.157645635548374!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd802ced08960c5%3A0x7b2406a342114256!2sPantai%20Goa%20Petapa!5e0!3m2!1sid!2sid!4v1717248902252!5m2!1sid!2sid" class="rounded-lg w-full h-full border-0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+                </iframe>
+              </div>
 
+              <div class="flex-row w-full">
+                {{-- Name --}}
+                <div class="w-full rounded-lg pb-2">
+                  <input type="text" name="nama_pemesan" id="nama_pemesan" class="bg-white shadow border-gray-300 text-gray-900 *:sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Masukkan Nama" required="">
+                </div>
+
+                {{-- Email --}}
+                <div class="w-full rounded-lg py-4">
+                  <input type="email" name="email_pemesan" id="email_pemesan" class="bg-white shadow border-gray-300 text-gray-900 *:sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Masukkan Email" required="">
+                </div>
+
+                {{-- Phone --}}
+                <div class="w-full rounded-lg pt-2">
+                  <input type="tel" name="telepon_pemesan" id="telepon_pemesan" class="bg-white shadow border-gray-300 text-gray-900 *:sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Masukkan Telepon" required="">
+                </div>
+              </div>
+            </div>
 
             {{-- GO BACK --}}
             <div class="mt-10 flex flex-col sm:flex-row">
-              <div class="w-1/6">
-                <a href="order"><svg class="w-7 h-7 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 10"><path stroke="currentColor" stroke-linecap="round" d="M13 5H1m0 0 4 4M1 5l4-4"/></svg>
+              <div class="w-1/2 flex items-center">
+                <a href="order" class="flex items-center">
+                  <svg class="w-7 h-7 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" d="M13 5H1m0 0 4 4M1 5l4-4"/>
+                  </svg>
+                  <h1 class="text-lg font-semibold leading-tight tracking-tight text-gray-900 md:text-lg dark:text-white ml-2">Kembali</h1>
+                </a>
               </div>
-              <div class="">
-                <h1 class="text-lg font-semibold leading-tight tracking-tight text-gray-900 md:text-lg dark:text-white"> Kembali</h1></a>
+              <div class="w-1/2 flex justify-end items-end">
+                <a href="payment"><button type="submit" class="w-full text-white hover bg-blue-900 shadow border-blue-900 hover:bg-blue-600 block focus:outline-none font-bold rounded-lg sm:text-sm px-5 p-2.5 text-center dark:bg-blue-200 dark:text-blue-900  dark:hover:bg-blue-600 dark:hover:text-white hover:text-white">Selanjutnya</button></a>
               </div>
-            </div>
+            </div>            
           </div>
         </div>
 

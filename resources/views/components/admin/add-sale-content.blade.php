fce8857c72d1b275dfcaf454db773b05d026202c
Irfan Dwi Samudra <140700419+irfandwisamudra@users.noreply.github.com>
2024-06-09 22:03:34 +0700

    Update add transaction content

--- resources/views/components/admin/add-sale-content.blade.php
+++ resources/views/components/admin/add-sale-content.blade.php
@@ -8,8 +8,10 @@
       <div class="grid grid-cols-12 gap-4">
         <div class="col-span-10">
           <select id="nama" name="nama" style="width: 100%" required>
+            <option value="" selected="">Pilih pengunjung</option>
             @foreach ($pengunjung as $p)
-              <option value="{{ $p->id }}">{{ $p->nama }}</option>
+              <option value="{{ $p->uuid }}">{{ $p->nama }} -
+                {{ $p->user->no_telepon ?? $p->guest->no_telepon }}</option>
             @endforeach
           </select>
         </div>
@@ -36,8 +38,8 @@
               required>
               <option value="" selected="">Pilih tiket</option>
               @foreach ($tiket as $t)
-                <option value="{{ $t->id }}">{{ $t->keterangan }} -
-                  Rp. {{ number_format($t->harga, 0, ',', '.') }}
+                <option value="{{ $t->id }}" data-harga="{{ $t->harga }}">{{ $t->keterangan }} -
+                  Rp{{ number_format($t->harga, 0, ',', '.') }}
                 </option>
               @endforeach
             </select>
@@ -56,7 +58,7 @@
               <input type="text" name="jumlah[]" id="jumlah-1" data-input-counter data-input-counter-min="1"
                 data-input-counter-max="50" aria-describedby="helper-text-explanation"
                 class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="999" value="1" required />
+                placeholder="999" required />
               <button type="button" id="increment-button-1" data-input-counter-increment="jumlah-1"
                 class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                 <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
@@ -88,6 +90,21 @@
               d="M12 7.757v8.486M7.757 12h8.486M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
           </svg>
         </button>
+      </div>
+    </div>
+    <div class="grid grid-cols-12 gap-4 mb-4">
+      <div class="col-span-12 sm:col-span-6">
+        <div class="mb-2 text-sm font-medium text-gray-900 dark:text-white">Total</div>
+        <div
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+          <span id="total">Loading...</span>
+        </div>
+      </div>
+      <div class="col-span-12 sm:col-span-6">
+        <label for="total_bayar" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Bayar</label>
+        <input type="number" name="total_bayar" id="total_bayar"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+          placeholder="20000" required />
       </div>
     </div>
   </div>
@@ -166,6 +183,38 @@
           $(this).closest('.ticket-row').remove();
         }
       });
+    });
+
+    $('#ticket-container').on('change', 'select', function() {
+      let total = 0;
+      $('#ticket-container select').each(function() {
+        let harga = $(this).find(':selected').data('harga');
+        let jumlah = $(this).closest('.ticket-row').find('input').val();
+        total += harga * jumlah;
+      });
+
+      if (isNaN(total)) {
+        total = 'Loading...';
+      } else {
+        total = 'Rp' + total.toLocaleString('id-ID');
+      }
+      $('#total').text(total);
+    });
+
+    $('#ticket-container').on('click', 'button', function() {
+      let total = 0;
+      $('#ticket-container select').each(function() {
+        let harga = $(this).find(':selected').data('harga');
+        let jumlah = $(this).closest('.ticket-row').find('input').val();
+        total += harga * jumlah;
+      });
+
+      if (isNaN(total)) {
+        total = 'Loading...';
+      } else {
+        total = 'Rp' + total.toLocaleString('id-ID');
+      }
+      $('#total').text(total);
     });
   });
 </script>

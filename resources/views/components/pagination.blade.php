1ae2e3816ce15041311a390f633731597f116ca6
NurulRizka <nurulrizka222@gmail.com>
2024-06-09 20:46:30 +0700

    update blog, blog-card, pagination

--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -2,7 +2,7 @@
   <ul class="flex items-center -space-x-px h-8 text-sm">
     <li>
       <a href="#"
-        class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-blue-500 bg-white border border-e-0 border-blue-300 rounded-s-lg hover:bg-blue-100">
+        class="flex items-center justify-center px-3 h-8 ms-0 leading-tight dark:bg-gray-800 text-blue-500 bg-white border border-e-0 border-blue-600 rounded-s-lg hover:bg-blue-100 dark:hover:bg-gray-900 dark:hover:text-blue-600">
         <span class="sr-only">Previous</span>
         <svg class="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
           fill="none" viewBox="0 0 6 10">
@@ -13,27 +13,28 @@
     </li>
     <li>
       <a href="#"
-        class="flex items-center justify-center px-3 h-8 leading-tight text-blue-500 bg-white border border-blue-300 hover:bg-blue-100">1</a>
+        class="flex items-center justify-center px-3 h-8 leading-tight text-blue-500 bg-white dark:bg-gray-800
+         border border-blue-600 hover:bg-blue-100 dark:hover:bg-gray-900 dark:hover:text-blue-600">1</a>
     </li>
     <li>
       <a href="#"
-        class="flex items-center justify-center px-3 h-8 leading-tight text-blue-500 bg-white border border-blue-300 hover:bg-blue-100">2</a>
+        class="flex items-center justify-center px-3 h-8 leading-tight text-blue-500 bg-white dark:bg-gray-800 border border-blue-600 hover:bg-blue-100 dark:hover:bg-gray-900 dark:hover:text-blue-600">2</a>
     </li>
     <li>
       <a href="#" aria-current="page"
-        class="z-10 flex items-center justify-center px-3 h-8 leading-tight text-blue-800 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-800/90">3</a>
+        class="z-10 flex items-center justify-center px-3 h-8 leading-tight text-blue-600 dark:bg-gray-900 border border-blue-600 bg-blue-50 hover:bg-blue-100 hover:text-blue-800">3</a>
     </li>
     <li>
       <a href="#"
-        class="flex items-center justify-center px-3 h-8 leading-tight text-blue-500 bg-white border border-blue-300 hover:bg-blue-100">4</a>
+        class="flex items-center justify-center px-3 h-8 leading-tight text-blue-500 bg-white dark:bg-gray-800 border border-blue-600 hover:bg-blue-100 dark:hover:bg-gray-900 dark:hover:text-blue-600">4</a>
     </li>
     <li>
       <a href="#"
-        class="flex items-center justify-center px-3 h-8 leading-tight text-blue-500 bg-white border border-blue-300 hover:bg-blue-100">5</a>
+        class="flex items-center justify-center px-3 h-8 leading-tight text-blue-500 bg-white dark:bg-gray-800 border border-blue-600 hover:bg-blue-100 dark:hover:bg-gray-900 dark:hover:text-blue-600">5</a>
     </li>
     <li>
       <a href="#"
-        class="flex items-center justify-center px-3 h-8 leading-tight text-blue-500 bg-white border border-blue-300 rounded-e-lg hover:bg-blue-100">
+        class="flex items-center justify-center px-3 h-8 leading-tight text-blue-500 bg-white dark:bg-gray-800 border border-blue-600 rounded-e-lg hover:bg-blue-100 dark:hover:bg-gray-900 dark:hover:text-blue-600">
         <span class="sr-only">Next</span>
         <svg class="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
           fill="none" viewBox="0 0 6 10">

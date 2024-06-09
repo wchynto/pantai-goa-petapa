92a9c929d2cc30a7939315b48599e4f91d50dd56
wchynto <wchynto.dev@gmail.com>
2024-06-09 14:07:02 +0700

    add statistic dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,19 +1,21 @@
 <x-admin.layout>
-  <x-slot:title>{{ $title }}</x-slot:title>
+    <x-slot:title>{{ $title }}</x-slot:title>
 
-  <div class="px-4 pt-6">
-    <div class="grid gap-4">
-      <div class="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800">
-        <div class="grid grid-cols-1 gap-4">
-          <div class="col">
-            <x-admin.visitor-statistics></x-admin.visitor-statistics>
-          </div>
+    <div class="px-4 pt-6">
+        <div class="grid gap-4">
+            <div
+                class="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+                <div class="grid grid-cols-1 gap-4">
+                    <div class="col">
+                        <x-admin.visitor-statistics></x-admin.visitor-statistics>
+                    </div>
 
-          <div class="col">
-            <x-admin.statistical-numbers></x-admin.statistical-numbers>
-          </div>
+                    <div class="col">
+                        <x-admin.statistical-numbers :tiket="$tiket" :pengunjung="$pengunjung"
+                            :users="$users"></x-admin.statistical-numbers>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </x-admin.layout>
--- resources/views/components/admin/statistical-numbers.blade.php
+++ resources/views/components/admin/statistical-numbers.blade.php
@@ -1,23 +1,23 @@
 <div
-  class="bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 dark:border-gray-700 sm:p-6 dark:bg-gray-800 px-4 py-8 lg:py-16 lg:px-6">
-  <div class="max-w-screen-xl mx-auto text-center">
-    <dl class="grid gap-8 sm:gap-0 mx-auto text-gray-900 sm:grid-cols-4 dark:text-white">
-      <div class="flex flex-col items-center justify-center">
-        <dt class="mb-2 text-3xl md:text-4xl font-extrabold">8,6 Jt</dt>
-        <dd class="font-light text-gray-500 dark:text-gray-400">Total Keuntungan</dd>
-      </div>
-      <div class="flex flex-col items-center justify-center">
-        <dt class="mb-2 text-3xl md:text-4xl font-extrabold">2.340</dt>
-        <dd class="font-light text-gray-500 dark:text-gray-400">Total Pengunjung</dd>
-      </div>
-      <div class="flex flex-col items-center justify-center">
-        <dt class="mb-2 text-3xl md:text-4xl font-extrabold">4</dt>
-        <dd class="font-light text-gray-500 dark:text-gray-400">Total Tiket</dd>
-      </div>
-      <div class="flex flex-col items-center justify-center">
-        <dt class="mb-2 text-3xl md:text-4xl font-extrabold">1.456</dt>
-        <dd class="font-light text-gray-500 dark:text-gray-400">Total Members</dd>
-      </div>
-    </dl>
-  </div>
+    class="bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 dark:border-gray-700 sm:p-6 dark:bg-gray-800 px-4 py-8 lg:py-16 lg:px-6">
+    <div class="max-w-screen-xl mx-auto text-center">
+        <dl class="grid gap-8 sm:gap-0 mx-auto text-gray-900 sm:grid-cols-4 dark:text-white">
+            <div class="flex flex-col items-center justify-center">
+                <dt class="mb-2 text-3xl md:text-4xl font-extrabold">8,6 Jt</dt>
+                <dd class="font-light text-gray-500 dark:text-gray-400">Total Keuntungan</dd>
+            </div>
+            <div class="flex flex-col items-center justify-center">
+                <dt class="mb-2 text-3xl md:text-4xl font-extrabold">{{ count($pengunjung) }}</dt>
+                <dd class="font-light text-gray-500 dark:text-gray-400">Total Pengunjung</dd>
+            </div>
+            <div class="flex flex-col items-center justify-center">
+                <dt class="mb-2 text-3xl md:text-4xl font-extrabold">{{ count($tiket) }}</dt>
+                <dd class="font-light text-gray-500 dark:text-gray-400">Total Tiket</dd>
+            </div>
+            <div class="flex flex-col items-center justify-center">
+                <dt class="mb-2 text-3xl md:text-4xl font-extrabold">{{ count($users) }}</dt>
+                <dd class="font-light text-gray-500 dark:text-gray-400">Total Members</dd>
+            </div>
+        </dl>
+    </div>
 </div>

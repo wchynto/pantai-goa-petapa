020d4f15b5955f54c63379b3dc1204aa94e82a98
Irfan Dwi Samudra <140700419+irfandwisamudra@users.noreply.github.com>
2024-06-15 14:44:29 +0700

    Update faker locale to Indonesian in .env.example and factories

--- database/factories/GuestFactory.php
+++ database/factories/GuestFactory.php
@@ -9,15 +9,15 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class GuestFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            'no_telepon' => $this->faker->e164PhoneNumber('id_ID'),
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      'no_telepon' => $this->faker->phoneNumber('id_ID'),
+    ];
+  }
 }

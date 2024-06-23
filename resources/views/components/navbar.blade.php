b5417640263beb757c85af2eead3037046043dc0
Irfan Dwi Samudra <140700419+irfandwisamudra@users.noreply.github.com>
2024-06-23 17:16:54 +0700

    Update navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -39,77 +39,75 @@
         </svg>
       </button>
 
-            @if (auth()->check())
-                <button type="button"
-                    class="flex text-sm rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                    id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
-                    data-dropdown-placement="bottom">
-                    <span class="sr-only">Open user menu</span>
-                    <img class="w-8 h-8 rounded-full shadow-lg"
-                        src="{{ auth()->user()->foto ? Storage::url(auth()->user()->foto) : asset('images/default-profile.png') }}"
-                        alt="User Photo">
-                </button>
-                <!-- Dropdown menu -->
-                <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-                    id="user-dropdown">
-                    <div class="px-4 py-3">
-                        <span
-                            class="block text-sm text-gray-900 dark:text-white">{{ auth()->user()->pengunjung->nama }}</span>
-                        <span
-                            class="block text-sm  text-gray-500 truncate dark:text-gray-400">{{ auth()->user()->email }}</span>
-                    </div>
-                    <ul class="py-2" aria-labelledby="user-menu-button">
-                        <li>
-                            <a href="{{ route('user.profil', auth()->user()->uuid) }}"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profil</a>
-                        </li>
-                        <li>
-                            <a href="{{ route('user.history-order', auth()->user()->uuid) }}"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Riwayat
-                                Pemesanan</a>
-                        </li>
-                        <li>
-                            <a href="{{ route('user.logout') }}"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Logout</a>
-                        </li>
-                    </ul>
-                </div>
-                <button data-collapse-toggle="navbar-user" type="button"
-                    class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                    aria-controls="navbar-user" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 17 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M1 1h15M1 7h15M1 13h15" />
-                    </svg>
-                </button>
-            @else
-                <a href="{{ url('login') }}"
-                    class="inline-flex justify-center items-center py-2 px-5 text-base font-medium text-center text-white rounded-lg bg-slate-500 hover:bg-slate-600 focus:ring-4 focus:ring-slate-100 dark:focus:ring-slate-700">
-                    Login
-                </a>
-            @endif
+      @if (auth()->check())
+        <button type="button"
+          class="flex text-sm rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+          id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
+          data-dropdown-placement="bottom">
+          <span class="sr-only">Open user menu</span>
+          <img class="w-8 h-8 rounded-full shadow-lg"
+            src="{{ auth()->user()->foto ? Storage::url(auth()->user()->foto) : asset('images/default-profile.png') }}"
+            alt="User Photo">
+        </button>
+        <!-- Dropdown menu -->
+        <div
+          class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+          id="user-dropdown">
+          <div class="px-4 py-3">
+            <span class="block text-sm text-gray-900 dark:text-white">{{ auth()->user()->pengunjung->nama }}</span>
+            <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">{{ auth()->user()->email }}</span>
+          </div>
+          <ul class="py-2" aria-labelledby="user-menu-button">
+            <li>
+              <a href="{{ route('user.profil', auth()->user()->uuid) }}"
+                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profil</a>
+            </li>
+            <li>
+              <a href="{{ route('user.history-order', auth()->user()->uuid) }}"
+                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Riwayat
+                Pemesanan</a>
+            </li>
+            <li>
+              <a href="{{ route('user.logout') }}"
+                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Logout</a>
+            </li>
+          </ul>
         </div>
-        <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
-            <ul
-                class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-6 lg:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li>
-                    <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
-                </li>
-                <li>
-                    <x-nav-link href="/tiket" :active="request()->is('tiket')">Tiket</x-nav-link>
-                </li>
-                <li>
-                    <x-nav-link href="/tentang" :active="request()->is('tentang')">Tentang</x-nav-link>
-                </li>
-                <li>
-                    <x-nav-link href="/kontak" :active="request()->is('kontak')">Kontak</x-nav-link>
-                </li>
-                <li>
-                    <x-nav-link href="/blog" :active="request()->is('blog')">Blog</x-nav-link>
-                </li>
-            </ul>
-        </div>
+        <button data-collapse-toggle="navbar-user" type="button"
+          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-user" aria-expanded="false">
+          <span class="sr-only">Open main menu</span>
+          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15" />
+          </svg>
+        </button>
+      @else
+        <a href="{{ url('login') }}"
+          class="inline-flex justify-center items-center py-2 px-5 text-base font-medium text-center text-white rounded-lg bg-slate-500 hover:bg-slate-600 focus:ring-4 focus:ring-slate-100 dark:focus:ring-slate-700">
+          Login
+        </a>
+      @endif
     </div>
+    <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
+      <ul
+        class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-6 lg:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <li>
+          <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+        </li>
+        <li>
+          <x-nav-link href="/tiket" :active="request()->is('tiket')">Tiket</x-nav-link>
+        </li>
+        <li>
+          <x-nav-link href="/tentang" :active="request()->is('tentang')">Tentang</x-nav-link>
+        </li>
+        <li>
+          <x-nav-link href="/kontak" :active="request()->is('kontak')">Kontak</x-nav-link>
+        </li>
+        <li>
+          <x-nav-link href="/blog" :active="request()->is('blog')">Blog</x-nav-link>
+        </li>
+      </ul>
+    </div>
+  </div>
 </nav>

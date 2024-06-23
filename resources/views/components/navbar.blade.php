ed85762664fb22b9f666d3648d7d15d7ff8a4561
Irfan Dwi Samudra <140700419+irfandwisamudra@users.noreply.github.com>
2024-06-23 13:32:09 +0700

    Update navbar link to user's order page

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -8,7 +8,7 @@
     </a>
     <div class="flex items-center md:order-2 space-x-3 rtl:space-x-reverse">
       @if (auth()->check())
-        <a href="transaksi">
+        <a href="{{ route('user.order', auth()->user()->uuid) }}">
           <button type="button"
             class="relative text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 rounded fill-gray-500 dark:fill-gray-400"

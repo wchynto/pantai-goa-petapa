f103ce08c39468ebe3c6f581e90b573209c75c4c
Irfan Dwi Samudra <140700419+irfandwisamudra@users.noreply.github.com>
2024-06-24 03:07:13 +0700

    Update order page and login for admin

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -9,7 +9,7 @@
       <!-- Card -->
       <div class="w-full max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow dark:bg-gray-800">
         <h2 class="text-2xl font-bold text-gray-900 dark:text-white">
-          Sign in to Admin Pantai Goa Petapa
+          Login ke Admin Pantai Goa Petapa
         </h2>
         @if (session()->has('error'))
           <x-alert.error>{{ session('error') }}</x-alert.error>
@@ -18,26 +18,25 @@
           @csrf
           @method('POST')
           <div>
-            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-              email</label>
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
             <input type="email" name="email" id="email"
               class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
               placeholder="name@company.com" required>
+            @error('email')
+              <span class="text-red-500 text-sm">{{ $message }}</span>
+            @enderror
           </div>
           <div>
-            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-              password</label>
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
             <input type="password" name="password" id="password" placeholder="••••••••"
               class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
               required>
-          </div>
-          <div class="flex items-start">
-            <a href="#" class="ml-auto text-sm text-primary-700 hover:underline dark:text-primary-500">Lost
-              Password?</a>
+            @error('password')
+              <span class="text-red-500 text-sm">{{ $message }}</span>
+            @enderror
           </div>
           <button type="submit"
-            class="w-full px-5 py-3 text-base font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login
-            to your account</button>
+            class="w-full px-5 py-3 text-base font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login</button>
         </form>
       </div>
     </div>
--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -1,133 +1,141 @@
 <x-layout>
-    @php
-        $total = 0;
-    @endphp
-    <x-slot:title>{{ $title }}</x-slot:title>
-    <section class="flex justify-center mt-16">
-        <div class="container xl:max-w-screen-xl p-4">
-            <div class="flex items-center justify-center">
-                <div
-                    class="relative inline-flex items-center justify-center w-16 h-16 overflow-hidden bg-blue-900 rounded-full">
-                    <span class="text-2xl font-semibold text-white ">1</span>
-                </div>
-
-                <hr class="w-12 h-1 my-4 bg-gray-700 border-0 rounded md:my-10">
-
-                <div>
-                    <div
-                        class="relative inline-flex items-center justify-center w-16 h-16 overflow-hidden bg-blue-100 rounded-full">
-                        <span class="text-2xl font-semibold text-white dark:text-blue-900">2</span>
-                    </div>
-                </div>
-
-                <hr class="w-12 h-1 my-4 bg-gray-700 border-0 rounded md:my-10">
-
-                <div>
-                    <div
-                        class="relative inline-flex items-center justify-center w-16 h-16 overflow-hidden bg-blue-100 rounded-full">
-                        <span class="text-2xl font-semibold text-white dark:text-blue-900">3</span>
-                    </div>
-                </div>
-            </div>
-            <div class="flex items-center justify-center mx-auto dark:text-white">
-                <h1 class="text-l px-14 font-bold">Pesan</h1>
-                <h1 class="text-l">Pembayaran</h1>
-                <h1 class="text-l px-8">Selesai</h1>
-            </div>
-
-            <form action="{{ route('proses-pembayaran') }}" method="post">
-                @csrf
-                @method('POST')
-                <div class="flex flex-row pt-20 gap-6 lg:gap-10">
-                    {{-- SHOPPING CART --}}
-                    <div class="w-1/2 md:w-3/4 mb-8">
-                        <h1 class="text-lg mb-4 font-semibold dark:text-white">Keranjang belanja</h1>
-                        <div
-                            class="w-full bg-white border border-gray-200 shadow-xl p-6 md:p-8 dark:bg-gray-800 dark:border-gray-600 mb-4 rounded-lg">
-                            <div class="relative overflow-x-auto shadow-xl sm:rounded-lg mb-4">
-                                <table
-                                    class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
-                                    <thead
-                                        class="text-xs text-gray-700 uppercase bg-blue-100 dark:bg-blue-900 dark:text-gray-300">
-                                        <tr>
-                                            <th scope="col" class="px-6 py-3 lg:text-sm">
-                                                Product
-                                            </th>
-                                            <th scope="col" class="px-6 py-3 lg:text-sm"></th>
-                                            <th scope="col" class="px-6 py-3 lg:text-sm">
-                                                Quantity
-                                            </th>
-                                            <th scope="col" class="px-6 py-3 lg:text-sm">
-                                                Subtotal
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach (Session::get('cart') ?? [] as $item)
-                                            @php
-                                                $total += $item['jumlah'] * $item['harga'];
-                                            @endphp
-                                            <input type="hidden" name="tiket[]" value="{{ $item['uuid'] }}">
-                                            <input type="hidden" name="jumlah[]" value="{{ $item['jumlah'] }}">
-                                            <input type="hidden" name="total_harga" value="{{ $total }}">
-                                            <tr
-                                                class="bg-white border-b dark:bg-gray-800 dark:border-gray-600 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                                <td class="px-6 py-4">
-                                                    <img src="{{ strpos($item['thumbnail'], 'tiket/thumbnails') ? Storage::url($item['thumbnail']) : asset($item['thumbnail']) }}" alt="{{ $item['keterangan'] }}">
-                                                </td>
-                                                <th scope="row"
-                                                    class="px-6 py-4 font-medium text-xs text-gray-900 whitespace-nowrap dark:text-white md:text-base">
-                                                    {{ $item['keterangan'] }}
-                                                </th>
-                                                <td class="px-6 py-4 text-xs md:text-base">
-                                                    {{ $item['jumlah'] }}
-                                                </td>
-                                                <td class="px-6 py-4 text-xs md:text-base">
-                                                    Rp.
-                                                    {{ number_format($item['jumlah'] * $item['harga'], 0, ',', '.') }}
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-
-                            {{-- GO BACK --}}
-                            <div class="mt-10 flex flex-row mx-4 lg:mx-0">
-                                <div class="w-1/2 flex items-center">
-                                    <a href="{{ route('home.tiket') }}" class="flex items-center">
-                                        <svg class="w-5 h-5 lg:w-5 lg:h-5 text-gray-900 dark:text-white"
-                                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 10">
-                                            <path stroke="currentColor" stroke-linecap="round"
-                                                d="M13 5H1m0 0 4 4M1 5l4-4" />
-                                        </svg>
-                                        <h1
-                                            class="text-base font-semibold leading-tight tracking-tight text-gray-900 md:text-lg dark:text-white ml-2">
-                                            Kembali</h1>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    {{-- ORDER SUM --}}
-                    <div class="w-1/2 md:w-1/4 pe-3 md:pe-0">
-                        <h1 class="text-lg font-semibold dark:text-white">Order Summary</h1>
-                        <div
-                            class="w-full bg-white border border-gray-200 shadow-xl lg:p-6 p-4 dark:bg-gray-800 dark:border-gray-600 mb-6 rounded-lg my-6 lg:mt-4">
-                            <h1
-                                class="text-sm lg:text-lg font-light leading-tight tracking-tight text-blue-950 md:text-base dark:text-white mb-2">
-                                Total Payment</h1></a>
-                            <h1
-                                class="text-sm lg:text-lg font-extrabold leading-tight tracking-tight text-blue-950 md:text-base dark:text-white">
-                                Rp. {{ number_format($total, 0, ',', '.') }}</h1></a>
-                        </div>
-                        <button type="submit"
-                            class="w-full text-white hover bg-blue-900 shadow border-blue-900 hover:bg-blue-600 block focus:outline-none font-bold rounded-lg text-xs lg:px-5 lg:p-2.5 p-3 text-center dark:bg-blue-900 dark:text-white  dark:hover:bg-blue-600 dark:hover:text-white hover:text-white md:text-base">Pesan
-                            Sekarang</button>
-                    </div>
-                </div>
-            </form>
+  @php
+    $total = 0;
+  @endphp
+  <x-slot:title>{{ $title }}</x-slot:title>
+  <section class="flex justify-center mt-16">
+    <div class="container xl:max-w-screen-xl p-4">
+      <div class="flex items-center justify-center">
+        <div class="relative inline-flex items-center justify-center w-16 h-16 overflow-hidden bg-blue-900 rounded-full">
+          <span class="text-2xl font-semibold text-white ">1</span>
         </div>
-    </section>
+
+        <hr class="w-12 h-1 my-4 bg-gray-700 border-0 rounded md:my-10">
+
+        <div>
+          <div
+            class="relative inline-flex items-center justify-center w-16 h-16 overflow-hidden bg-blue-100 rounded-full">
+            <span class="text-2xl font-semibold text-white dark:text-blue-900">2</span>
+          </div>
+        </div>
+
+        <hr class="w-12 h-1 my-4 bg-gray-700 border-0 rounded md:my-10">
+
+        <div>
+          <div
+            class="relative inline-flex items-center justify-center w-16 h-16 overflow-hidden bg-blue-100 rounded-full">
+            <span class="text-2xl font-semibold text-white dark:text-blue-900">3</span>
+          </div>
+        </div>
+      </div>
+      <div class="flex items-center justify-center mx-auto dark:text-white">
+        <h1 class="text-l px-14 font-bold">Pesan</h1>
+        <h1 class="text-l">Pembayaran</h1>
+        <h1 class="text-l px-8">Selesai</h1>
+      </div>
+
+      <form action="{{ route('proses-pembayaran') }}" method="post">
+        @csrf
+        @method('POST')
+        <div class="flex flex-row pt-20 gap-6 lg:gap-10">
+          {{-- SHOPPING CART --}}
+          <div class="w-1/2 md:w-3/4 mb-8">
+            <h1 class="text-lg mb-4 font-semibold dark:text-white">Keranjang belanja</h1>
+            <div
+              class="w-full bg-white border border-gray-200 shadow-xl p-6 md:p-8 dark:bg-gray-800 dark:border-gray-600 mb-4 rounded-lg">
+              <div class="relative overflow-x-auto shadow-xl sm:rounded-lg mb-4">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
+                  <thead class="text-xs text-gray-700 uppercase bg-blue-100 dark:bg-blue-900 dark:text-gray-300">
+                    <tr>
+                      <th scope="col" class="px-6 py-3 lg:text-sm">
+                        Product
+                      </th>
+                      <th scope="col" class="px-6 py-3 lg:text-sm"></th>
+                      <th scope="col" class="px-6 py-3 lg:text-sm">
+                        Quantity
+                      </th>
+                      <th scope="col" class="px-6 py-3 lg:text-sm">
+                        Subtotal
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @if (!Session::get('cart'))
+                      <tr
+                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-600 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td class="px-6 py-4 text-xs md:text-base" colspan="4">
+                          <h1 class="text-center">Keranjang belanja kosong</h1>
+                        </td>
+                      </tr>
+                    @endif
+                    @foreach (Session::get('cart') ?? [] as $item)
+                      @php
+                        $total += $item['jumlah'] * $item['harga'];
+                      @endphp
+                      <input type="hidden" name="tiket[]" value="{{ $item['uuid'] }}">
+                      <input type="hidden" name="jumlah[]" value="{{ $item['jumlah'] }}">
+                      <input type="hidden" name="total_harga" value="{{ $total }}">
+                      <tr
+                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-600 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td class="px-6 py-4">
+                          <img
+                            src="{{ strpos($item['thumbnail'], 'tiket/thumbnails') ? Storage::url($item['thumbnail']) : asset($item['thumbnail']) }}"
+                            alt="{{ $item['keterangan'] }}">
+                        </td>
+                        <th scope="row"
+                          class="px-6 py-4 font-medium text-xs text-gray-900 whitespace-nowrap dark:text-white md:text-base">
+                          {{ $item['keterangan'] }}
+                        </th>
+                        <td class="px-6 py-4 text-xs md:text-base">
+                          {{ $item['jumlah'] }}
+                        </td>
+                        <td class="px-6 py-4 text-xs md:text-base">
+                          Rp.
+                          {{ number_format($item['jumlah'] * $item['harga'], 0, ',', '.') }}
+                        </td>
+                      </tr>
+                    @endforeach
+                  </tbody>
+                </table>
+              </div>
+
+              {{-- GO BACK --}}
+              <div class="mt-10 flex flex-row mx-4 lg:mx-0">
+                <div class="w-1/2 flex items-center">
+                  <a href="{{ route('home.tiket') }}" class="flex items-center">
+                    <svg class="w-5 h-5 lg:w-5 lg:h-5 text-gray-900 dark:text-white" aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 10">
+                      <path stroke="currentColor" stroke-linecap="round" d="M13 5H1m0 0 4 4M1 5l4-4" />
+                    </svg>
+                    <h1
+                      class="text-base font-semibold leading-tight tracking-tight text-gray-900 md:text-lg dark:text-white ml-2">
+                      Kembali</h1>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {{-- ORDER SUM --}}
+          <div class="w-1/2 md:w-1/4 pe-3 md:pe-0">
+            <h1 class="text-lg font-semibold dark:text-white">Order Summary</h1>
+            <div
+              class="w-full bg-white border border-gray-200 shadow-xl lg:p-6 p-4 dark:bg-gray-800 dark:border-gray-600 mb-6 rounded-lg my-6 lg:mt-4">
+              <h1
+                class="text-sm lg:text-lg font-light leading-tight tracking-tight text-blue-950 md:text-base dark:text-white mb-2">
+                Total Payment</h1></a>
+              <h1
+                class="text-sm lg:text-lg font-extrabold leading-tight tracking-tight text-blue-950 md:text-base dark:text-white">
+                Rp. {{ number_format($total, 0, ',', '.') }}</h1></a>
+            </div>
+            @if (Session::get('cart'))
+              <button type="submit"
+                class="w-full text-white hover bg-blue-900 shadow border-blue-900 hover:bg-blue-600 block focus:outline-none font-bold rounded-lg text-xs lg:px-5 lg:p-2.5 p-3 text-center dark:bg-blue-900 dark:text-white  dark:hover:bg-blue-600 dark:hover:text-white hover:text-white md:text-base">Pesan
+                Sekarang</button>
+            @endif
+          </div>
+        </div>
+      </form>
+    </div>
+  </section>
 </x-layout>
